f1e3cb9ce1af1197e09ca71bdf4fedff5ed0b33a
α⅃βε <albertodeprezzo@gmail.com>
2021-07-26 10:50:06 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zreq3b/goliame
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,53 @@
+package goliame
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/smtp"
+)
+
+// Msg is a struct to hold email information
+type Msg struct {
+	From   string
+	To     string
+	Passwd string
+	Host   string
+	Port   string
+	Body   []byte
+}
+
+// Send message, passing email parameters
+func (m *Msg) Send() (bool, error) {
+	auth := smtp.PlainAuth("", m.From, m.Passwd, m.Host)
+	uri := fmt.Sprintf("%s:%s", m.Host, m.Port)
+
+	if m.Body != nil {
+		err := smtp.SendMail(uri, auth, m.From, []string{m.To}, []byte(m.Body))
+
+		if err != nil {
+			log.Println(err)
+			return false, err
+		}
+		return true, nil
+	}
+
+	// If body is nil, you cannot send email: we need to read file first
+	err := errors.New("no email body")
+	return false, err
+}
+
+// ReadFile fetch file content in order to build email body
+func (m *Msg) ReadFile(filename string) error {
+	// build file content object
+	fcontent, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	// assigne to field's struct
+	m.Body = fcontent
+
+	return nil
+}
